1fd081bce8952fbac0aecfd9a7a78a60d2337639
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2022-09-11 16:30:06 -0300

    Exercício de C#
    Exercício de C# finalizados

--- 3° ADS/Carol C#/ConsoleApp1/ConsoleApp1/Program.cs
+++ 3° ADS/Carol C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,15 @@
-﻿using System.IO;
+﻿using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
+using System.Threading.Channels;
 using static System.Net.Mime.MediaTypeNames;
 
 internal class Program
 {
+    private static DateTime data_nasc;
+
     private static void Main(string[] args)
     {
         int a = 0;
@@ -11,6 +18,8 @@ internal class Program
         //1 - Analise os algoritmos (representados em Diagrama de Chapin) abaixo e diga o que será 
         //impresso na tela ao serem executados:
         //A)
+        Console.WriteLine("1 -Elaborar um programa para cada algoritmo " +
+            "(representados em Diagrama de Chapin) do item 1");
         Console.WriteLine("Exercício (A)");
         a = 10;
         b = 20;
@@ -94,7 +103,8 @@ internal class Program
 
         //2 – Elabore um algoritmo que calcule a média da idade de 5 alunos.
         Console.WriteLine("\nExercício (2)");
-        Console.WriteLine("Montar um programa que calcule a média da idade de 5 alunos e exiba o nome de cada aluno com sua idade e a média da idade dos alunos.");
+        Console.WriteLine("Montar um programa que calcule a média da idade de 5 alunos e " +
+            "exiba o nome de cada aluno com sua idade e a média da idade dos alunos.");
 
         int med = 0;
         int idade = 0;
@@ -127,21 +137,90 @@ internal class Program
         }
 
         //3 – Crie um algoritmo que verifique se um número informado é par ou ímpar.
-        Console.WriteLine("Exercício 3");
+        Console.WriteLine("\nExercício 3");
+        Console.WriteLine("\nCrie um algoritmo que verifique se um número informado é par ou ímpar.");
+
+        Console.WriteLine("Digite um numero inteiro");
+        x = int.Parse(Console.ReadLine());
+        int par = 0;
+
+        par = x % 2;
+        if (par == 0)
+        {
+            Console.WriteLine($" O numero {x} é par");
+        }
+        else
+        {
+            Console.WriteLine($" O numero {x} é impar");
+        }
+
+        //4 – Escreva um programa para ler as dimensões de um
+        //retângulo(base e altura), calcular e escrever a área
+        //do retângulo. (ar = b * a)
+        Console.WriteLine("\nExercício 4");
+        Console.WriteLine("Escreva um programa para ler as dimensões " +
+            "de um retângulo (base e altura), calcular e escrever a área do " +
+            "retângulo. (ar = b*a )\n");
+
+        Console.WriteLine("Informe a altura do retângulo");
+        float al = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informa a base do retângulo");
+        float bs = float.Parse(Console.ReadLine());
+
+        float ar = al * bs;
+
+        Console.WriteLine($"A área do retângulo informado é {ar}");
+
+        /*
+         * 5 – Faça um programa que leia a idade de uma pessoa expressa em anos, 
+         * meses e dias e escreva a idade dessa pessoa expressa apenas em dias. 
+         * Considerar ano com 365 dias e mês com 30 dias.
+         */
+        Console.WriteLine("\nExercício 5");
+        Console.WriteLine("Faça um programa que leia a idade de uma pessoa " +
+            "expressa em anos, meses e dias e escreva a idade dessa pessoa " +
+            "expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.\n");
+
+        DateTime data = DateTime.Today;
+        Console.WriteLine("Diegite a data do seu nascimento ex: 00/00/0000");
+        data_nasc = Convert.ToDateTime(Console.ReadLine());
+
+        TimeSpan id_atual = data - data_nasc;
+        string[] split = Convert.ToString(id_atual).Split('.');
 
 
+        int ano_nasc = int.Parse(data_nasc.ToString("yyyy"));
+        int ano_atual = int.Parse(data.ToString("yyyy"));
+
+        idade = ano_atual - ano_nasc;
+
+        Console.WriteLine($"Você tem anos {idade} de idade e {split[0]} dias de vida");
 
 
+        /*
+         * 6 – Faça um programa que exiba quantas pessoas possuem mais de 18 anos. 
+         * O algoritmo deverá ler a idade de 10 pessoas.
+         */
+        Console.WriteLine("\nExercício 6");
+        Console.WriteLine("Faça um programa que exiba quantas pessoas possuem mais " +
+            "de 18 anos. O algoritmo deverá ler a idade de 10 pessoas.");
+        x = 0;
+        y = 0;
 
-
-
-
-
-
-
-
-
-
-
+        for (i = 0;i <10;i++ )
+        {
+            Console.WriteLine($"Informe a idade da {i+1}° pessoa");
+            idade = int.Parse(Console.ReadLine());
+            if (idade >= 18)
+            {
+                x++;
+            }
+            else
+            {
+                y++;
+            }
+        }
+        Console.WriteLine($"O total de pessoas maior que 18 anos é = {x}");
+        Console.WriteLine($"O totla de pessoas menor que 18 anos é = {y}");
     }
 }
