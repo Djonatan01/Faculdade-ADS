45d8fad637081851a9cf2c59517c11d060113fd1
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2022-10-28 18:14:03 -0300

    Atualização de pasta

---
+++ 3° ADS/Carol C#/Console/2022-10-27_POO_Conteudo10_Exercicio1/2022-10-27_POO_Conteudo10_Exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using _2022_10_27_POO_Conteudo10_Exercicio1;
+
+namespace Exercicio1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            double largura, altura;
+            Console.WriteLine("Entre com a largura do retangulo: ");
+            largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entre com a altura do retangulo: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("A area do retangulo é: {0:F2}", retangulo.getArea(largura, altura));
+            Console.WriteLine("O perímetro do retangulo é: {0:F2}", retangulo.getPerimetro(largura, altura));
+            Console.WriteLine("A diagonal do retangulo é: {0:F2}", retangulo.getDiagonal(largura, altura));
+        }
+    }
+}
---
+++ 3° ADS/Carol C#/Console/2022-10-27_POO_Conteudo10_Exercicio1/2022-10-27_POO_Conteudo10_Exercicio1/Retangulo.cs
@@ -0,0 +1,36 @@
+﻿namespace _2022_10_27_POO_Conteudo10_Exercicio1
+{
+    
+    internal class Retangulo
+    {
+        private double resultado;
+
+        public Retangulo()
+        {           
+            resultado = 0;           
+        }
+
+        public double getArea(double largura, double altura)
+        {
+            resultado = largura * altura;
+            return resultado;
+            
+        }
+
+        public double getPerimetro(double largura, double altura)
+        {
+            resultado = 2*(largura + altura);
+            return resultado;
+
+        }
+
+        public double getDiagonal(double largura, double altura)
+        {
+            resultado = Math.Sqrt(Math.Pow(largura,2) + Math.Pow(altura,2));
+            return resultado;
+
+        }
+
+
+    }
+}
