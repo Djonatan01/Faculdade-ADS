bac0a4f310c336889e889e5b5eecf8236cb7b334
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2022-10-29 18:37:24 -0300

    Windows form
    Criação de exercício no Windows form

---
+++ 3° ADS/Carol C#/Windows Form/Poo_Conteudo10_exercicios/Poo_Conteudo10_exercicios/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poo_Conteudo10_exercicios
+{
+    public partial class Exercico1 : Form
+    {
+        public Exercico1()
+        {
+            InitializeComponent();
+        }
+
+        private void btArea_Click(object sender, EventArgs e)
+        {
+            lbArea.Text = "";
+            lbDiagonal.Text = "";
+            lbPerimetro.Text = "";
+
+            Exe1 exerc1 = new Exe1();
+            lbArea.Text = "Total da área é {0:F2}" + Convert.ToString(+
+                exerc1.getArea(Convert.ToDouble(txt_Largura.Text),+
+                Convert.ToDouble(txt_altura.Text)));
+
+
+            lbDiagonal.Text = "";
+            lbPerimetro.Text = "";
+
+        }
+
+        private void btPerimetro_Click(object sender, EventArgs e)
+        {
+
+            lbArea.Text = "";
+            lbDiagonal.Text = "";
+            lbPerimetro.Text = "";
+
+            Exe1 exerc1 = new Exe1();
+            lbPerimetro.Text = "Total da perímetro é " + Convert.ToString(+
+                exerc1.getPerimetro(Convert.ToDouble(txt_Largura.Text), +
+                Convert.ToDouble(txt_altura.Text)));
+
+            lbDiagonal.Text = "";
+            lbArea.Text = "";
+        }
+
+        private void btDiagonal_Click(object sender, EventArgs e)
+        {
+
+            lbArea.Text = "";
+            lbDiagonal.Text = "";
+            lbPerimetro.Text = "";
+
+            Exe1 exerc1 = new Exe1();
+            lbDiagonal.Text = ("Total da diagonal é {0:F2}" + 
+                exerc1.getDiagonal(Convert.ToDouble(txt_Largura.Text), +
+                Convert.ToDouble(txt_altura.Text)));
+
+            lbPerimetro.Text = "";
+            lbArea.Text = "";
+        }
+
+        private void Exercico1_Load(object sender, EventArgs e)
+        {
+            txt_altura.Text = "";
+            txt_Largura.Text = "";
+            lbArea.Text = "";
+            lbDiagonal.Text = "";
+            lbPerimetro.Text = "";
+        }
+    }
+}
---
+++ 3° ADS/Carol C#/Windows Form/Poo_Conteudo10_exercicios/Poo_Conteudo10_exercicios/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poo_Conteudo10_exercicios
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            DialogResult retono = MessageBox.Show("SIM = Para exrcício 1  \n" +
+                "Não = para exrcício 2 ","Opção de esclolha",MessageBoxButtons.YesNoCancel); 
+            if(retono == DialogResult.Yes) { 
+                Application.Run(new Exercico1());
+            }
+            else
+            {
+                Application.Run(new Exercicio2() );
+            }
+        }
+    }
+}
